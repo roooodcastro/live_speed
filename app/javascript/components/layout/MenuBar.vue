ed4e2a36da31dfc0d0b00d22b65f57500e5aa51e
Rodrigo Castro <rod.c.azevedo@gmail.com>
2019-02-22 09:11:33 +0000

    Added logo to menu bar

--- app/javascript/components/layout/MenuBar.vue
+++ app/javascript/components/layout/MenuBar.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="menu-bar">
+    <a
+      href="/"
+      class="menu-logo"
+    >
+      <img
+        alt="Live Speed Logo"
+        :src="require('images/logo_text.svg')"
+      >
+    </a>
     <slot />
   </div>
 </template>
@@ -12,7 +21,21 @@
   @import 'stylesheets/_variables.scss';
 
   .menu-bar {
-    background: rgba($brand, 0.4);
-    height: 4rem;
+    background:  rgba($brand, 0.4);
+    display:     flex;
+    align-items: center;
+    height:      4rem;
+  }
+
+  .menu-logo {
+    display:     flex;
+    height:      100%;
+    align-items: center;
+    padding:     0 1rem;
+    transition:  all 0.15s;
+
+    img { height: 3rem; width: 10rem; }
+
+    &:hover { background-color: rgba($brand-white, 0.1); }
   }
 </style>
