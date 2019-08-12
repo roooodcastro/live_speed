dfd10c0f28c41f9622223ca56261cd7ceebfa999
Rodrigo Castro <rod.c.azevedo@gmail.com>
2019-08-12 16:44:03 +0100

    Add bundle analiser tool to analise webpack bundle file size

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,11 @@ const { environment, } = require('@rails/webpacker');
 const erb =  require('./loaders/erb');
 const vue =  require('./loaders/vue');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+if (process.env.BUNDLE_ANALYZER) {
+  environment.plugins.prepend('BundleAnalyzerPlugin', new BundleAnalyzerPlugin());
+}
 
 environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin());
 environment.loaders.append('vue', vue);
